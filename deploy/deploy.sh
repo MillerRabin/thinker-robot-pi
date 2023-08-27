853d5de61f3a0cbe7ffd42ec6cc8eac0befa68f7
Vladimir Aseev <nemesisv@mail.ru>
2023-08-27 16:50:19 +0200

    add tasks for automated debugging

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,9 +2,17 @@ APP_DIR=/home/ci/app/thinker-robot-pi
 HOST=192.168.1.22
 USER=ci
 
+echo "Cleaning up host dist folder"
 rm -rf ./dist
+echo "Creating deploy archive"
 mkdir ./dist
 tar -czvf ./dist/dist.tar.gz ./src package.json
+echo "Cleaning up $HOST dist folder"
 ssh -i ./.keys/user-key $USER@$HOST "rm -rf $APP_DIR/dist && mkdir -p $APP_DIR/dist"
+echo "Uploading deploy archive to $HOST"
 scp -pr -i ./.keys/user-key ./dist/dist.tar.gz ci@192.168.1.22:$APP_DIR/dist/
-ssh -i ./.keys/user-key $USER@$HOST "cd $APP_DIR && tar -xzf ./dist/dist.tar.gz --directory $APP_DIR && node --inspect=0.0.0.0 ./src/main.js"
+echo "Extracting archive at $HOST"
+ssh -i ./.keys/user-key $USER@$HOST "cd $APP_DIR && tar -xzf ./dist/dist.tar.gz --directory $APP_DIR"
+echo "Launching app at $HOST"
+ssh -i ./.keys/user-key $USER@$HOST "cd $APP_DIR && node --inspect=0.0.0.0 ./src/main.js"
+echo "Finished"
